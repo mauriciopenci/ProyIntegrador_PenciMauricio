75972f2b16fd1cafca2ad8e0af1fcb50105153d2
mauriciopenci <100880895+mauriciopenci@users.noreply.github.com>
2023-05-21 23:56:30 -0300

    .

--- src/app/service/persona.service.ts
+++ src/app/service/persona.service.ts
@@ -2,16 +2,18 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { persona } from '../model/persona.model';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
-export class PersonaService {
-  URL = 'http://localhost:8080/personas/'
 
-  constructor(private http:HttpClient ) { }
-  
-  public getPersona(): Observable <persona> {
-    return this.http.get<persona>(this.URL+'traer/perfil')
+export class PersonaService {
+  URL = environment.URL + 'personas/';
+
+  constructor(private http: HttpClient) { }
+
+  public getPersona(): Observable<persona>{
+    return this.http.get<persona>(this.URL+ 'traer/perfil');
   }
 }
